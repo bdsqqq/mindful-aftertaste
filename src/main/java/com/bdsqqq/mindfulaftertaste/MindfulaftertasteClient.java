ccb57800f12293de7188173bb4a939135c4b140e
Igor Bedesqui <igorbedesqui@gmail.com>
2025-04-10 20:14:28 +0100

    first commit

---
+++ src/main/java/com/bdsqqq/mindfulaftertaste/MindfulaftertasteClient.java
@@ -0,0 +1,10 @@
+package com.bdsqqq.mindfulaftertaste;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MindfulaftertasteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello, World!");
+    }
+}
