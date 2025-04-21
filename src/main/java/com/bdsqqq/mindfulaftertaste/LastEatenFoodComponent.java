563adaea020f7c31c569727512bb57e5e75997a7
Igor Bedesqui <igorbedesqui@gmail.com>
2025-04-21 21:54:16 +0100

    Revert "vibeeeee"
    This reverts commit 5adcb1a30916b16814f90a98b3bfdecdc94d898e.

--- src/main/java/com/bdsqqq/mindfulaftertaste/LastEatenFoodComponent.java
+++
@@ -1,9 +0,0 @@
-package com.bdsqqq.mindfulaftertaste;
-
-import dev.onyxstudios.cca.api.v3.component.Component;
-import net.minecraft.util.Identifier;
-
-public interface LastEatenFoodComponent extends Component {
-    Identifier getLastFoodGroup();
-    void setLastFoodGroup(Identifier group);
-} 
--- src/main/java/com/bdsqqq/mindfulaftertaste/Mindfulaftertaste.java
+++ src/main/java/com/bdsqqq/mindfulaftertaste/Mindfulaftertaste.java
@@ -1,28 +1,17 @@
 package com.bdsqqq.mindfulaftertaste;
 
 import net.fabricmc.api.ModInitializer;
-import dev.onyxstudios.cca.api.v3.component.ComponentKey;
-import dev.onyxstudios.cca.api.v3.component.ComponentRegistry;
-import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
-import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
-import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.Identifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Mindfulaftertaste implements ModInitializer, EntityComponentInitializer {
+public class Mindfulaftertaste implements ModInitializer {
 	public static final String MOD_ID = "mindful-aftertaste";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	public static final ComponentKey<LastEatenFoodComponent> LAST_EATEN_FOOD_COMPONENT_KEY =
-		ComponentRegistry.getOrCreate(new Identifier(MOD_ID, "last_eaten_food"), LastEatenFoodComponent.class);
 
 	@Override
 	public void onInitialize() {
@@ -31,10 +20,5 @@ public class Mindfulaftertaste implements ModInitializer, EntityComponentInitial
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-	}
-
-	@Override
-	public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
-		registry.registerFor(LivingEntity.class, LAST_EATEN_FOOD_COMPONENT_KEY, LastEatenFoodComponentImpl::new);
 	}
 }
