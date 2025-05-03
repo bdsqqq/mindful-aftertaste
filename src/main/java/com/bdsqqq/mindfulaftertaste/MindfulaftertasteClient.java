a42baaaaf98003847a27ac8e9fce812ce6ba765d
Igor Bedesqui <igorbedesqui@gmail.com>
2025-05-04 00:56:20 +0100

    Revert "these needed renaming? god I hate case sensitivity in file names"
    This reverts commit b04305b81d1708df2576ca3258abb0f86ffe4893.

--- src/main/java/com/bdsqqq/mindfulaftertaste/MindfulaftertasteClient.java
+++ src/main/java/com/bdsqqq/mindfulaftertaste/MindfulaftertasteClient.java
@@ -2,7 +2,7 @@ package com.bdsqqq.mindfulaftertaste;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class MindfulAftertasteClient implements ClientModInitializer {
+public class MindfulaftertasteClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         System.out.println("Mindful Aftertaste Client Initialized");
