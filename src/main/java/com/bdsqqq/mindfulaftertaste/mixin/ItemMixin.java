a6f3d94a00815bb75448458d5d39c98f293e093f
Igor Bedesqui <igorbedesqui@gmail.com>
2025-05-04 05:04:37 +0100

    remove unused

--- src/main/java/com/bdsqqq/mindfulaftertaste/mixin/ItemMixin.java
+++ src/main/java/com/bdsqqq/mindfulaftertaste/mixin/ItemMixin.java
@@ -3,7 +3,6 @@ package com.bdsqqq.mindfulaftertaste.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.entity.LivingEntity;
